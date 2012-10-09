525b8a0ae10ea1601e6db4ecc10bfa1342aeaea5
Ruben d'Arco <cyclops@prof-x.net>
2012-10-09 22:55:25 +0200

    Add own except filter to allow links. Finally.

--- wp-content/themes/NOMAC/functions.php
+++ wp-content/themes/NOMAC/functions.php
@@ -161,5 +161,26 @@ function get_post_thumb($postId)
 }
 
 
+function improved_trim_excerpt($text) {
+        global $post;
+        if ( '' == $text ) {
+                $text = get_the_content('');
+                $text = apply_filters('the_content', $text);
+                $text = str_replace('\]\]\>', ']]&gt;', $text);
+                $text = preg_replace('@<script[^>]*?>.*?</script>@si', '', $text);
+                $text = strip_tags($text, '<a>');
+                $excerpt_length = theme_excerpt_length();
+                $words = explode(' ', $text, $excerpt_length + 1);
+                if (count($words)> $excerpt_length) {
+                        array_pop($words);
+                        array_push($words, "...");
+                        $text = implode(' ', $words);
+                }
+        }
+        return $text;
+}
+remove_filter('get_the_excerpt', 'wp_trim_excerpt');
+add_filter('get_the_excerpt', 'improved_trim_excerpt');
 
 
+?>
