763250f65a67a67b5ce54e222d427876a4e8d3e5
Ruben d'Arco <cyclops@prof-x.net>
2013-12-04 18:23:03 +0100

    Updated styling and added result table for meeting.

--- wp-content/plugins/nomac/admin/rulechange.php
+++ wp-content/plugins/nomac/admin/rulechange.php
@@ -6,8 +6,8 @@ function admin_nomac_rulechange() {
 
     echo '<div class="wrap">';
     screen_icon('users');
-    echo '<h2>NOMAC reglemenswijzigingen Beheer</h2>';
-    echo '<p>Op deze pagina worden de reglemenswijzigingen beheerd.</p>';
+    echo '<h2 class="noprint">NOMAC reglemenswijzigingen Beheer</h2>';
+    echo '<p class="noprint">Op deze pagina worden de reglemenswijzigingen beheerd.</p>';
 
 
 	if (!isset($_SESSION['LIC_YEAR'])) {
@@ -48,11 +48,11 @@ function admin_nomac_rulechange() {
 	}
 	echo '</select>';
 	echo '<input class="button-secondary" type="submit" name="do" value="Selecteer Jaar" />';
-	echo '</form>';	
 
 	echo "Lijst voor het seizoen/jaar " . $_SESSION['LIC_YEAR'];
 	echo "<br />";
 
+	echo '</form>';	
 	admin_nomac_rulechangelist($_SESSION['LIC_YEAR']);
 	echo "</div>";
 
@@ -74,9 +74,6 @@ function admin_nomac_rulechangelist($year) {
 		$out .= '<form method="post" action="">';		
 		foreach ($rows as $row) {
 			if ($row->Class != $class) {
-				if ($class != "") {
-					$out .= "</table>";
-				}
 				$i=0;
 				$class = $row->Class;
 				$out .= '<h3 class="pagebreak">' . stripslashes($row->Class) . '</h3>';
@@ -86,31 +83,36 @@ function admin_nomac_rulechangelist($year) {
 			}
 			$i++;
 
-			$out .= '<table class="wp-list-table left">';
-			$out .= '<tr class="noprint"><th colspan="1">Verwijderen:</th><td colspan="3"><input type="checkbox" name="delete['.$row->Id.']" /></td></tr>'; //4
-			$out .= '<tr><th>Voorstel nummer:</th><td colspan="3">' . $i . '</td></tr>'; 
+			$out .= '<table class="ruletable">';
+			$out .= '<tr><th>Voorstel nummer:</th><th colspan="3">' . $i . '</th></tr>'; 
+			$out .= '<tr class="noprint"><td colspan="1">Verwijderen:</td><td colspan="3"><input type="checkbox" name="delete['.$row->Id.']" /></td></tr>'; //4
 			$out .= '<tr>';
-			$out .= '<th>Ingediend door:</th><td>' . stripslashes($row->SubmittedBy) . '</td>'; // 2
-			$out .= '<th>E-mail:</th><td>' . stripslashes($row->SubmitterEmail) . '</td>';// 2
+			$out .= '<td>Ingediend door:</td><td>' . stripslashes($row->SubmittedBy) . '</td>'; // 2
+			$out .= '<td>E-mail:</td><td>' . stripslashes($row->SubmitterEmail) . '</td>';// 2
 			$out .= '</tr>';
 			$out .= '<tr>';
-			$out .= '<th>Ingediend op:</th><td>' . $row->SubmittedOn . '</td>'; // 2
-			$out .= '<th>Klasse:</th><td>' . stripslashes($row->Class) . '</td>';// 2
+			$out .= '<td>Ingediend op:</td><td>' . $row->SubmittedOn . '</td>'; // 2
+			$out .= '<td>Klasse:</td><td>' . stripslashes($row->Class) . '</td>';// 2
 			$out .= '</tr>';
 			$out .= '<tr>';
-			$out .= '<th>Pagina:</th><td>' . $row->Page . '</td>'; // 2
-			$out .= '<th>Artikel:</th><td>' . stripslashes($row->Article) . '</td>';// 2
+			$out .= '<td>Pagina:</td><td>' . $row->Page . '</td>'; // 2
+			$out .= '<td>Artikel:</td><td>' . stripslashes($row->Article) . '</td>';// 2
 			$out .= '</tr>';
-			$out .= '<tr><th colspan="4">Oude tekst:</th></tr>';
+			$out .= '<tr><th colspan="4"><br />Oude tekst:</th></tr>';
 			$out .= '<tr><td colspan="4">' . cleanOutput($row->OldText) . '</td></tr>';
-			$out .= '<tr><th colspan="4">Nieuwe tekst:</th></tr>';
+			$out .= '<tr><th colspan="4"><br />Nieuwe tekst:</th></tr>';
 			$out .= '<tr><td colspan="4">' . cleanOutput($row->NewText) . '</td></tr>';
-			$out .= '<tr><th colspan="4">Uitleg:</th></tr>';
+			$out .= '<tr><th colspan="4"><br />Uitleg:</th></tr>';
 			$out .= '<tr><td colspan="4">' . cleanOutput($row->Comment) . '</td></tr>';
+			$out .= '<tr class="printonly"><th colspan="4"><br />Stemming/uitslag</th></tr>';
+			$out .= '<tr class="printonly"><td>Voorstel teruggetrokken:</td><td>Stemmen voor:</td><td>Stemmen tegen:</td><td>Stemmen onthouden:</td></tr>';
+			$out .= '<tr class="printonly"><td>Ja/Nee</td><td></td><td></td><td></td></tr>';
+			$out .= '<tr class="printonly"><td colspan="4">Aanpassingen op het voorstel/het uiteindelijke voorstel:</td></tr>';
+			$out .= '<tr class="printonly"><td colspan="4"><br/><br /><br /><br/><br/><br/><br/<br/></td></tr>';
 			$out .= "</table>";
 		}
 		
-		$out .= '<input type="submit" class="button-primary noprint" name="do" value="Aangevinkte items verwijderen" />';
+		$out .= '<p class="submit noprint"><input type="submit" class="button-primary noprint" name="do" value="Aangevinkte items verwijderen" /></p>';
 		$out .= '</form>';
 
 	}
